2b6bbc9b7a56f3a30e75a5a93c6de326cec158f9
Andreas Bauer <bauerei@gmail.com>
2013-09-11 11:42:20 +0200

    Homecontroller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Spruchomat::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'home#index'
   # root 'welcome#index'
 
   # Example of regular route:
