ad5d100648d7245dc23db08c517df86e2fbf2293
Andreas Bauer <bauerei@gmail.com>
2013-09-13 12:42:35 +0200

    Show poster thumbnail in admin table, form and show action.

--- app/admin/poster.rb
+++ app/admin/poster.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register Poster do
   index do
+    column :image do |poster|
+      image_tag(poster.url.thumb)
+    end
     column :text
     column :party
     default_actions
@@ -9,9 +12,23 @@ ActiveAdmin.register Poster do
     link_to(image_tag(poster.url.thumb), admin_poster_path(poster))
   end
 
+  show do
+    attributes_table do
+      row :id
+      row :party
+      row :image do
+        image_tag(poster.url.thumb)
+      end
+      row :text
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
   form(html: {multipart: true}) do |f|
     f.inputs "Poster Details" do
-      f.input :url, as: :file
+      f.input :url, as: :file, hint: f.template.image_tag(f.object.url.thumb)
       f.input :text
       f.input :party
     end
