6118f647a31cbcae5358207a2f52c5e810893435
Andreas Bauer <bauerei@gmail.com>
2013-09-14 21:56:12 +0200

    Symlink upload dir while deploying.

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,11 @@ namespace :deploy do
   task :symlink_shared do
     run "ln -nfs #{etc_path}/database.yml #{release_path}/config/database.yml"
   end
+
+  task :symlink_upload_dir do
+    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
+  end
 end
 
 after 'deploy:update_code', 'deploy:symlink_shared'
+after 'deploy:update_code', 'deploy:symlink_upload_dir'
