198c71042fd26d5d6c1835eb9a81e0bad3366aa5
Caspar Clemens Mierau <ccm@screenage.de>
2013-09-16 21:56:26 +0200

    changing to new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :current_stage,  'production'
-set :deploy_to,      "/home/plakate/apps/#{application}-#{current_stage}"
-set :domain,         "volans.uberspace.de"
+set :deploy_to,      "/home/wahlplakatomat/apps/#{application}-#{current_stage}"
+set :domain,         "wahlplakatomat.de"
 set :user,           "plakate"
-set :etc_path,       "/home/plakate/etc"
+set :etc_path,       "/home/wahlplakatomat/etc"
 set :passenger_port, "17321"
 set :passenger_cmd,  "source $HOME/.bash_profile && TMPDIR=#{current_path}/tmp/ bundle exec passenger"
 
-role :app, "volans.uberspace.de"
-role :web, "volans.uberspace.de"
-role :db,  "volans.uberspace.de", :primary => true
+role :app, "78.46.91.204"
+role :web, "78.46.91.204"
+role :db,  "78.46.91.204", :primary => true
