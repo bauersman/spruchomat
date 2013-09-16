bc8f53a74605d874bfd19fd5d7331d33d9d46285
Caspar Clemens Mierau <ccm@screenage.de>
2013-09-16 22:06:48 +0200

    fixing user name in capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :current_stage,  'production'
 set :deploy_to,      "/home/wahlplakatomat/apps/#{application}-#{current_stage}"
 set :domain,         "wahlplakatomat.de"
-set :user,           "plakate"
+set :user,           "wahlplakatomat"
 set :etc_path,       "/home/wahlplakatomat/etc"
 set :passenger_port, "17321"
 set :passenger_cmd,  "source $HOME/.bash_profile && TMPDIR=#{current_path}/tmp/ bundle exec passenger"
