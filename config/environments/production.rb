3e3d95a35390ca75d92748a4945c641ed0efaab3
Andreas Bauer <bauerei@gmail.com>
2014-04-28 19:49:54 +0200

    Change asset host to eu2014-assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Spruchomat::Application.configure do
   config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://assets.wahlplakatomat.de"
+  config.action_controller.asset_host = "http://eu2014-assets.wahlplakatomat.de"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
